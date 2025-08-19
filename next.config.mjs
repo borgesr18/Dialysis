45fdd7cfef0d69474b7b2f47b11b4c4f86ee2b3e
borgesr18 <borgesr18@yahoo.com.br>
2025-08-19 19:22:30 -0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: { allowedOrigins: ['https://dialysis-jk5e.vercel.app'] },
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000',
+        '127.0.0.1:3000',
+        process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : '',
+        process.env.APP_URL || '',
+      ].filter(Boolean),
+    },
   },
 };
+
 export default nextConfig;
